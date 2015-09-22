c4489cbae0a86bd981b39ee25f147f129b790058
mdobson2 <svc-student@RC335G05.student.directory.jccc>
2015-09-22 18:49:23 -0500

    My game

---
+++ Lab-04-something-game/Assets/Scripts/ModDataScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+
+public class ModDataScript : MonoBehaviour {
+
+    FileInfo originalFile;
+    TextAsset textFile;
+    TextReader reader;
+    ScriptEngine engine;
+
+    public List<string> questions = new List<string>();
+    public List<string> answers = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+
+        engine = Camera.main.GetComponent<ScriptEngine>();
+        
+        originalFile = new FileInfo(Application.dataPath + "/questions.txt");
+
+        if(originalFile != null && originalFile.Exists)
+        {
+            reader = originalFile.OpenText(); 
+        }
+        else
+        {
+            textFile = (TextAsset)Resources.Load("embedded", typeof(TextAsset));
+            reader = new StringReader(textFile.text);
+        }
+
+        string lineOfText;
+        int lineNumber = 0;
+
+        while((lineOfText = reader.ReadLine()) != null)
+        {
+            if(lineNumber % 2 == 0)
+            {
+                questions.Add(lineOfText);
+            }
+            else
+            {
+                answers.Add(lineOfText);
+            }
+            lineNumber++;
+        }
+
+        engine.RunGame(questions, answers);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Lab-04-something-game/Assets/Scripts/ScriptEngine.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine.UI;
+
+public class ScriptEngine : MonoBehaviour {
+
+    public Text questionText;
+    public Text statusText;
+
+    int numCorrect = 0;
+
+    List<string> questions;
+    List<string> answers;
+    int numDisplay = 0;
+    string answer;
+
+	public void RunGame(List<string> pQuestions, List<string> pAnswers)
+    {
+        questions = pQuestions;
+        answers = pAnswers;
+
+        NextQuestion();
+    }
+
+    public void NextQuestion()
+    {
+        questionText.text = questions[numDisplay];
+        statusText.text = "Number Right: " + numCorrect;
+    }
+
+    public void TrueAnswer()
+    {
+        answer = "true";
+        CheckAnswer();
+    }
+
+    public void FalseAnswer()
+    {
+        answer = "false";
+        CheckAnswer();
+    }
+
+    public void CheckAnswer()
+    {
+        if(answer == answers[numDisplay].ToLower())
+        {
+            numCorrect++;
+        }
+        numDisplay++;
+        NextQuestion();
+    }
+}
